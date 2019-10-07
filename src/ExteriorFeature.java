83301408a12910bb049577bd3aeaa4aa3dee4851
dms91 <drk7891@gmail.com>
2019-10-07 15:16:07 -0400

    Added ExteriorFeature class

---
+++ src/ExteriorFeature.java
@@ -0,0 +1,35 @@
+public class ExteriorFeature implements Feature {
+
+  private String exteriorFeature;
+
+  public ExteriorFeature() {
+    this.exteriorFeature = "Generic";
+  }
+
+  public ExteriorFeature(String exteriorFeature) {
+    this.exteriorFeature = exteriorFeature;
+  }
+
+  @Override
+  public String getFeature() {
+    return this.exteriorFeature;
+  }
+
+  @Override
+  public void setFeature(String exteriorFeature) {
+    this.exteriorFeature = exteriorFeature;
+  }
+
+  @Override
+  public String toString() {
+    return "Exterior [" + getFeature() + "]";
+  }
+
+  public static void main(String[] args) {
+    ExteriorFeature defaultFeature = new ExteriorFeature();
+    System.out.println(defaultFeature.toString());
+
+    ExteriorFeature definedFeature = new ExteriorFeature("Fog Lamps");
+    System.out.println(definedFeature.toString());
+  }
+}
